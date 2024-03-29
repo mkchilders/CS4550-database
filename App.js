ddcb9ade8bb6ce265f2c4c840dfa4de2634731bb
Sea-queue <chengqian1214@gmail.com>
2024-03-28 20:22:24 -0400

    set up the backend

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.listen(4000);
 
 app.get("/kanbas/hello", (req, res) => {
+  console.log("gets here");
   res.send("Hello from Seaqueue");
 });
